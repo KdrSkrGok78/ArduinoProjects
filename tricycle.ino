e59b6fbaf384771bc081410e9e1d86f6999cef66
Kadir Şükrü Gök <83140154+KdrSkrGok78@users.noreply.github.com>
2025-01-16 20:13:59 +0300

    tricycle.ino

--- tricycle.ino
+++ tricycle.ino
@@ -1,26 +1,26 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
-char state;
-int speed = 255;
-const int pinTwo = 6;  // in3(ileri glb)
+char state; //araca gelecek komutlar bu değişkene atanacak
+int speed = 255; //motorların maks hızı olacak şekilde ayalarlandı 0-255 arası değerler verilebilir.
+const int pinTwo = 6;  // in3 (output 1-2 için In1-3 ; output 3-4 için In2-4)
 const int pinOne = 5; // in1
-Servo frontServo;
-SoftwareSerial BTSerial(2,3);
+Servo frontServo; //servo motoru tanımladık
+SoftwareSerial BTSerial(2,3); // 2-> RX , 3->TX ; arduinoya ters bağlanacak
 
 void setup() {
-  frontServo.attach(9);  // Servo PWM pin 10
+  frontServo.attach(9);  // Servo PWM pin 9
   frontServo.write(90);   // Başlangıçta tekerleği düz konuma getir
   pinMode(pinTwo, OUTPUT);
   pinMode(pinOne, OUTPUT);
   Serial.begin(9600);
-  BTSerial.begin(9600); 
+  BTSerial.begin(9600);// BT modülü HC-06 9600 baud rate kabul ediyor bu yüzden bu hız seçildi
 }
 
 void loop() {
     if (BTSerial.available() > 0) {
-    state = BTSerial.read();
-    Serial.print("Received from BT: "); // Kontrol için Serial Monitor'e yaz
-    Serial.println(state);
+    state = BTSerial.read();//alınan komutu yaz
+    Serial.print("Received from BT: "); 
+    Serial.println(state);// Kontrol için Serial Monitor'e yaz
   }
   
   switch(state) {
